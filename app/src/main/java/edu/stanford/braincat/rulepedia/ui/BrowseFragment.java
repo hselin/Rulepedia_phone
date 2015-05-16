909dd440f05512f4243d5874a87d6a2bf4fafff3
hselin <hselin.chen@gmail.com>
2015-05-15 23:25:39 -0700

    Add google play

--- app/src/main/java/edu/stanford/braincat/rulepedia/ui/BrowseFragment.java
+++ app/src/main/java/edu/stanford/braincat/rulepedia/ui/BrowseFragment.java
@@ -2,9 +2,12 @@ package edu.stanford.braincat.rulepedia.ui;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +16,12 @@ import android.webkit.JavascriptInterface;
 import android.webkit.ValueCallback;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,6 +40,13 @@ import edu.stanford.braincat.rulepedia.model.RuleDatabase;
 import edu.stanford.braincat.rulepedia.model.Trigger;
 import edu.stanford.braincat.rulepedia.service.Callback;
 import edu.stanford.braincat.rulepedia.service.RuleExecutor;
+
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.request.OnDataPointListener;
+import com.google.android.gms.fitness.request.SensorRequest;
 
 
 /**
@@ -174,7 +190,11 @@ public class BrowseFragment extends Fragment {
 
         try {
             JSONObject jsonRule = (JSONObject) new JSONTokener(ruleJSON).nextValue();
+
+
+
             executor.installRule(jsonRule, new Callback<Rule>() {
+
                 private void getChannels(Trigger trigger, Collection<Channel> ctx) {
                     if (trigger instanceof CompositeTrigger) {
                         for (Trigger t : ((CompositeTrigger) trigger).getChildren())
@@ -186,11 +206,13 @@ public class BrowseFragment extends Fragment {
 
                 @Override
                 public void run(final Rule result, Exception error) {
-                    if (result != null) {
+                    if (result == null) {
                         reportInstallationSuccess(result);
 
                         getActivity().runOnUiThread(new Runnable() {
                             public void run() {
+
+                                /*
                                 Collection<ObjectPool.Object> placeholders =  result.getPlaceholders();
 
                                 Collection<Channel> channels = new ArrayList<>();
@@ -206,6 +228,10 @@ public class BrowseFragment extends Fragment {
 
 
                                 }
+                                */
+
+                                //GFitness gf = new GFitness(getActivity());
+                                GFitness_OLD gfo = new GFitness_OLD(getActivity(), getActivity().getApplicationContext());
                             }
                         });
 
@@ -220,6 +246,177 @@ public class BrowseFragment extends Fragment {
         } catch (JSONException e) {
             Log.e(LOG_TAG, "Failed to parse rule JSON: " + e.getMessage());
             reportInstallationError(e);
+        }
+    }
+
+
+    public class GFitness extends FragmentActivity
+            implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnDataPointListener {
+        private static final int REQUEST_OAUTH = 1001;
+        private GoogleApiClient mGoogleApiClient;
+        private Activity activity;
+
+        public GFitness(Activity activity){
+            this.activity = activity;
+            create();
+        }
+
+        public void create()
+        {
+            mGoogleApiClient = new GoogleApiClient.Builder(activity.getApplicationContext())
+                    .addApi(Fitness.SENSORS_API)  // Required for SensorsApi calls
+                            // Optional: specify more APIs used with additional calls to addApi
+                    .useDefaultAccount()
+                    .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+
+            mGoogleApiClient.connect();
+        }
+
+        @Override
+        public void onConnected(Bundle connectionHint) {
+            // Connected to Google Fit Client.
+            Log.d("!!!!!!!!!!!!!!!!", "@@@@@@@@@@@@@@@@@@2");
+
+            Fitness.SensorsApi.add(
+                    mGoogleApiClient,
+                    new SensorRequest.Builder()
+                            .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                            .build(),
+                    this);
+        }
+
+        @Override
+        public void onDataPoint(DataPoint dataPoint) {
+            // Do cool stuff that matters.
+        }
+
+        @Override
+        public void onConnectionSuspended(int cause) {
+            // The connection has been interrupted. Wait until onConnected() is called.
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult result) {
+            // Error while connecting. Try to resolve using the pending intent returned.
+            Log.d("!!!!!!!!!!!!!!!!", "%%%%%%%%%%%%%%%%%");
+            if (result.getErrorCode() == FitnessStatusCodes.NEEDS_OAUTH_PERMISSIONS) {
+                try {
+                    result.startResolutionForResult(this, REQUEST_OAUTH);
+                } catch (IntentSender.SendIntentException e) {
+                }
+            }
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_OAUTH && resultCode == RESULT_OK) {
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+
+
+
+    public class GFitness_OLD {
+        private static final int REQUEST_OAUTH = 1;
+        private Context ctx;
+        private Activity activity;
+
+        public GFitness_OLD(Activity activity, Context ctx){
+            /*
+            if (savedInstanceState != null) {
+                authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+            }
+            */
+
+            this.activity = activity;
+            this.ctx = ctx;
+
+            buildFitnessClient();
+        }
+
+        /**
+         *  Track whether an authorization activity is stacking over the current activity, i.e. when
+         *  a known auth error is being resolved, such as showing the account chooser or presenting a
+         *  consent dialog. This avoids common duplications as might happen on screen rotations, etc.
+         */
+        private static final String AUTH_PENDING = "auth_state_pending";
+        private boolean authInProgress = false;
+
+        private GoogleApiClient mClient = null;
+
+
+
+
+        /**
+         *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+         *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
+         *  (see documentation for details). Authentication will occasionally fail intentionally,
+         *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+         *  can address. Examples of this include the user never having signed in before, or having
+         *  multiple accounts on the device and needing to specify which account to use, etc.
+         */
+        private void buildFitnessClient() {
+            // Create the Google API Client
+            mClient = new GoogleApiClient.Builder(ctx)
+                    .addApi(Fitness.SENSORS_API)
+                    .addScope(new Scope(Scopes.FITNESS_LOCATION_READ))
+                    .addConnectionCallbacks(
+                            new GoogleApiClient.ConnectionCallbacks() {
+
+                                @Override
+                                public void onConnected(Bundle bundle) {
+                                    Log.i("!", "Connected!!!");
+                                    // Now you can make calls to the Fitness APIs.
+                                    // Put application specific code here.
+                                }
+
+                                @Override
+                                public void onConnectionSuspended(int i) {
+                                    // If your connection to the sensor gets lost at some point,
+                                    // you'll be able to determine the reason and react to it here.
+                                    if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                        Log.i("!", "Connection lost.  Cause: Network Lost.");
+                                    } else if (i == GoogleApiClient.ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                        Log.i("!", "Connection lost.  Reason: Service Disconnected");
+                                    }
+                                }
+                            }
+                    )
+                    .addOnConnectionFailedListener(
+                            new GoogleApiClient.OnConnectionFailedListener() {
+                                // Called whenever the API client fails to connect.
+                                @Override
+                                public void onConnectionFailed(ConnectionResult result) {
+                                    Log.i("!", "Connection failed. Cause: " + result.toString());
+                                    if (!result.hasResolution()) {
+                                        // Show the localized error dialog
+                                        GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+                                                activity, 0).show();
+                                        return;
+                                    }
+                                    // The failure has a resolution. Resolve it.
+                                    // Called typically when the app is not yet authorized, and an
+                                    // authorization dialog is displayed to the user.
+                                    if (!authInProgress) {
+                                        try {
+                                            Log.i("!", "Attempting to resolve failed connection");
+                                            authInProgress = true;
+                                            result.startResolutionForResult(activity,
+                                                    REQUEST_OAUTH);
+                                        } catch (IntentSender.SendIntentException e) {
+                                            Log.e("!",
+                                                    "Exception while starting resolution activity", e);
+                                        }
+                                    }
+                                }
+                            }
+                    )
+                    .build();
         }
     }
 
