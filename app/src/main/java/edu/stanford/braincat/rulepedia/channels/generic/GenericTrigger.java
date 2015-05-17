5ec5d2a42bb900a753e90a1ee79d67a821b1459b
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-05-16 20:27:11 -0700

    Fix timers

--- app/src/main/java/edu/stanford/braincat/rulepedia/channels/generic/GenericTrigger.java
+++ app/src/main/java/edu/stanford/braincat/rulepedia/channels/generic/GenericTrigger.java
@@ -105,12 +105,10 @@ public class GenericTrigger implements Trigger {
             for (Map.Entry<String, EventSource> e : eventSources.entrySet()) {
                 EventSource source = e.getValue();
 
-                if (source.checkEvent()) {
-                    if (source instanceof WebPollingEventSource)
-                        ScriptableObject.putProperty(newEventSourceValues, e.getKey(), Util.readString(((WebPollingEventSource) source).getLastConnection()));
-                    else
-                        ScriptableObject.putProperty(newEventSourceValues, e.getKey(), source.checkEvent());
-                }
+                if (source instanceof WebPollingEventSource)
+                    ScriptableObject.putProperty(newEventSourceValues, e.getKey(), Util.readString(((WebPollingEventSource) source).getLastConnection()));
+                else
+                    ScriptableObject.putProperty(newEventSourceValues, e.getKey(), source.checkEvent());
             }
 
             eventSourceValues = newEventSourceValues;
