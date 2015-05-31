d8fdaa5d666bec16a626545fd93cb00d25ee10d9
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-05-30 18:24:24 -0700

    Fix random quotes
    They bother me

--- app/src/main/java/edu/stanford/braincat/rulepedia/omletUI/RandomQuotes.java
+++ app/src/main/java/edu/stanford/braincat/rulepedia/omletUI/RandomQuotes.java
@@ -6,7 +6,7 @@ import java.util.Random;
  * Created by braincat on 5/29/15.
  */
 public class RandomQuotes {
-    static String[] quotes = {
+    private static final String[] quotes = {
             "The clear star that is yesterday is omni-present, much like candy.",
             "A token of gratitude shoots pineapples with a machinegun.",
             "A flailing monkey is always a pleasure.",
@@ -15,9 +15,10 @@ public class RandomQuotes {
             "Nothing of importance likes to have a shower in the morning.",
             "Another day tests the thesis that your theorem would unleash."
     };
+    private static final Random random = new Random();
 
     public static String getQuote()
     {
-        return quotes[new Random().nextInt(quotes.length)];
+        return quotes[random.nextInt(quotes.length)];
     }
 }
